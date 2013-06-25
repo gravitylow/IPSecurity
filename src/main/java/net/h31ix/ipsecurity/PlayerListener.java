05b799f94acff36534b31384cf1cbb75cb3f20cf
SonarBeserk <sonarbeserk@gmail.com>
2013-06-25 04:09:27 -0400

    Attempt to try again with ip when using hostnames
    , Also slipping into a new style of indention

--- src/main/java/net/h31ix/ipsecurity/PlayerListener.java
+++ src/main/java/net/h31ix/ipsecurity/PlayerListener.java
@@ -25,94 +25,109 @@ public class PlayerListener implements Listener
         Player player = event.getPlayer();
         String name = player.getName();
         InetAddress address = null;
-        try
-        {
+        try {
             address = InetAddress.getByName(player.getAddress().getAddress().toString().replace("/", ""));
         	
             String pip = null;
             
-            if(address.getHostName() == null)
-            {
+            if(address.getHostName() == null) {
             	pip = player.getAddress().getAddress().toString().replace("/", "");
-            } else
-            {
+            } else {
             	pip = address.getHostName().replace("/", "");
             }
             
-            if(player.isOp())
-            {
+            if(player.isOp()) {
                 player.setOp(false);
             }
+            
             List<String> ips = plugin.getIps(name);
             
-            if(ips != null)
-            {
+            if(ips != null) {
             	List<String> expectedIps = new ArrayList<String>();
             	
-            	for(String ip: ips)
-            	{
-            		if(!ip.equalsIgnoreCase("none"))
-            		{
-            			if(!pip.startsWith(ip)) 
-                                {
-                                    expectedIps.add(ip); 
-                                    continue;
-                                }
+            	for(String ip: ips) {
+            		if(!ip.equalsIgnoreCase("none")) {
+            			if(!pip.startsWith(ip)) {
+            				expectedIps.add(ip); 
+                            continue;
+                        }
             			
-            			if(plugin.isOp(name))
-            			{
+            			if(plugin.isOp(name)) {
             				player.setOp(true);
             			}
             			
             			return;
             		}
-            		else
-            		{
+            		else {
             			error(player, ip, pip, 2);
             			return;
             		}
             	}
             	
-            	for(String s: expectedIps)
-            	{
+            	if(!(address.getHostName() == null)) {
+            		for(String s: expectedIps) {
+                		error(player, s, pip, 1);
+                		continue;
+                	}
+            	}
+            	
+            	// set as the actual address in the case of hostnames
+            	pip = player.getAddress().getAddress().toString().replace("/", "");
+            	
+            	//second attempt using the true ip
+            	
+            	for(String ip: ips) {
+            		if(!ip.equalsIgnoreCase("none")) {
+            			if(!pip.startsWith(ip)) {
+            				expectedIps.add(ip); 
+                            continue;
+                        }
+            			
+            			if(plugin.isOp(name)) {
+            				player.setOp(true);
+            			}
+            			
+            			return;
+            		}
+            		else {
+            			error(player, ip, pip, 2);
+            			return;
+            		}
+            	}
+            	
+            	for(String s: expectedIps) {
             		error(player, s, pip, 1);
             		continue;
             	}
             }  
-        } catch (UnknownHostException e)
-        {
+        } catch (UnknownHostException e) {
         }
     }
     
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerQuit(PlayerQuitEvent event)
-    {
+    public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        if(player.isOp())
-        {
+        if(player.isOp()) {
             player.setOp(false);
         }
     }
     
     private void error(Player player, String expected, String given, int type)
     {
-        if(plugin.ban())
-        {
+        if(plugin.ban()) {
             String ip = player.getAddress().getAddress().toString();
             plugin.getServer().banIP(ip);
         }
+        
         player.kickPlayer(plugin.getReason());
         
-        if(plugin.alert())
-        {
+        if(plugin.alert()) {
         	plugin.getLogger().info("********************************");
-        	if(type == 1)
-        	{
+        	if(type == 1) {
         		plugin.getLogger().info("ALERT: "+player.getName()+" logged in with ip: "+given);
         		plugin.getLogger().info("But ip: "+expected+" was expected.");
         	}
-        	else if (type == 2)
-        	{
+        	else if (type == 2) {
         		plugin.getLogger().info("ALERT: "+player.getName()+" tried to log in, but is not allowed.");
         	}
         	plugin.getLogger().info("********************************");
